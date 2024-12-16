b8d23bb800cad8672599584232cb1fcf5f4ebfe6
Amran <124218@swcsd.us>
2024-12-16 14:37:01 -0500

    Initial Commit

---
+++ TheBrewBarn/TheBrewBarnApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheBrewBarnApp.swift
+//  TheBrewBarn
+//
+//  Created by Amran Abdullahi on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TheBrewBarnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheBrewBarnTests/TheBrewBarnTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheBrewBarnTests.swift
+//  TheBrewBarnTests
+//
+//  Created by Amran Abdullahi on 12/16/24.
+//
+
+import Testing
+@testable import TheBrewBarn
+
+struct TheBrewBarnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
